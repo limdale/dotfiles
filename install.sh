f9f9d8901763b32ac2e7a655ce955c1f2466160a
Dale Lim <dalelim.94@gmail.com>
2021-06-07 16:34:46 +1000

    Fix brew install jdk, macos cmd for tap and press-and-hold

--- install.sh
+++ install.sh
@@ -7,7 +7,10 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install brew stuff
+# Have to untap openjdk first in order to prevent tap conflicts:
+# https://github.com/AdoptOpenJDK/homebrew-openjdk/issues/106
+brew untap adoptopenjdk/openjdk
 brew bundle
 
 # Karabiner config
-cp karabiner/karabiner.json ~/.config/karabiner 
+cp karabiner/karabiner.json ~/.config/karabiner/karabiner.json
--- macos.sh
+++ macos.sh
@@ -1,5 +1,12 @@
 # Big Sur 11.4
 # you can know what defaults changes where by doing defaults read
 # do defaults read then change the pref then do defaults read again then diff the two
-defaults -currentHost write -g  com.apple.driver.AppleBluetoothMultitouch.trackpad.Clicking -int 1
-defaults -currentHost write -g com.apple.swipescrolldirection -bool NO
+
+# Tap to click
+defaults -currentHost write -g com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
+# Disable "natural" scrolling direction
+defaults -currentHost write -g com.apple.swipescrolldirection -bool false
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults -currentHost write -g NSGlobalDomain ApplePressAndHoldEnabled -bool false
