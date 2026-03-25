27bd64e0f715bd41cf03fce8963b8bd257079213
Dale Lim <dalelim.94+github@gmail.com>
2026-03-25 22:05:56 +1100

    WIP - update mac, separate work

---
+++ CHECKLIST.md
@@ -0,0 +1,31 @@
+## Install
+
+### Dev tools
+- [ ] git
+- [ ] JDK
+- [ ] Docker
+- [ ] Node/npm
+- [ ] Rust
+- [ ] Android Studio
+- [ ] VS Code
+- [ ] zsh
+- [ ] vim
+- [ ] mitmproxy
+- [ ] ffmpeg
+- [ ] jadx
+
+### Surfing
+- [ ] Firefox
+- [ ] Proton VPN
+
+### Chat apps
+- [ ] Telegram
+- [ ] Discord
+
+### Misc
+- [ ] Karabiner Elements
+- [ ] VLC
+
+### Optional
+- [ ] Chrome
+- [ ] Docker
--- install.sh
+++ install.sh
@@ -4,15 +4,24 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv zsh)"
 
-# Install brew stuff
-# Have to untap openjdk first in order to prevent tap conflicts:
-# https://github.com/AdoptOpenJDK/homebrew-openjdk/issues/106
-brew untap adoptopenjdk/openjdk
+# Install NVM and Node 16
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+nvm install 16
+
+# Install Rustup for Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install everything else via Brewfile
 brew bundle
 
+# Install Android Studio, open this in browser: https://developer.android.com/studio
+
 # Karabiner config
+echo "Need to initialise the Karabiner Elements app. Do the ff:"
+echo "1. Open the Karabiner Elements app"
+echo "2. Grant the required permissions"
+read -p "Once finished, press any key to proceed to copying over the karabiner.json file..." -n 1 -r
 cp karabiner/karabiner.json ~/.config/karabiner/karabiner.json
